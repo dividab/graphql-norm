0f3abc116eafdd3e0aa19c85fe7b75fa805a3f04
Jonathan Mourtada <jonathan.mourtada@divid.se>
2018-08-06 15:59:34 +0200

    Load denormalize tests dynamic

--- test/shared-test-data.ts
+++ test/shared-test-data.ts
@@ -1,11 +1,4 @@
-import * as fs from "fs";
+import { loadTests } from "./load-tests";
 import { OneTest } from "./shared-tests/one-test";
 
-const testBasePath = __dirname + "/shared-tests/";
-const importedTests = fs
-  .readdirSync(testBasePath)
-  .filter(f => f.match(/\.test\.ts$/i))
-  .map(f => require(testBasePath + f))
-  .map(importedModule => importedModule.test as OneTest);
-
-export const tests = importedTests;
+export const tests = loadTests<OneTest>("shared-tests/");
