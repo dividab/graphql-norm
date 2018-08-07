c3a4144f2ec0f37f229e39f33c8ac25f6d6962fd
Jonathan Mourtada <jonathan.mourtada@divid.se>
2018-08-07 14:57:14 +0200

    Add performance testing

---
+++ performance-test/performance-test.ts
@@ -0,0 +1,63 @@
+import * as BenchMark from "benchmark";
+import { normalize } from "../src/normalize";
+import { denormalize } from "../src/denormalize";
+import * as ArrayOfStringTest from "../test/shared-tests/query-with-array-of-string.test";
+import * as NamedFragmentsTest from "../test/shared-tests/with-named-fragments.test";
+import * as WithVariablesSimpleNestedList from "../test/shared-tests/with-variables-simple-nested-object.test";
+
+const normalizeSuite = new BenchMark.Suite();
+
+normalizeSuite
+  .add("Array of string test", function() {
+    const { query, variables, response } = ArrayOfStringTest.test;
+    normalize(query, variables, response);
+  })
+  .add("NamedFragmentsTest", function() {
+    const { query, variables, response } = NamedFragmentsTest.test;
+    normalize(query, variables, response);
+  })
+  .add("WithVariablesSimpleNestedList", function() {
+    const { query, variables, response } = WithVariablesSimpleNestedList.test;
+    normalize(query, variables, response);
+  })
+  .on("complete", function(this: any) {
+    // tslint:disable-next-line:no-invalid-this
+    console.log("normalization");
+    for (const b of this) {
+      const name: string = b.name;
+      const stats: BenchMark.Stats = b.stats;
+      console.log(name, stats.mean * 1000 + "ms");
+    }
+
+    console.log();
+    console.log();
+  })
+  .run({ async: true });
+
+const denormalizeSuit = new BenchMark.Suite();
+denormalizeSuit
+  .add("Array of string test", function() {
+    const { query, variables, entities } = ArrayOfStringTest.test;
+    denormalize(query, variables, entities);
+  })
+  .add("NamedFragmentsTest", function() {
+    const { query, variables, entities } = NamedFragmentsTest.test;
+    denormalize(query, variables, entities);
+  })
+  .add("WithVariablesSimpleNestedList", function() {
+    const { query, variables, entities } = WithVariablesSimpleNestedList.test;
+    denormalize(query, variables, entities);
+  })
+  .on("complete", function(this: any) {
+    // tslint:disable-next-line:no-invalid-this
+    console.log("denormalization");
+    for (const b of this) {
+      const name: string = b.name;
+      const stats: BenchMark.Stats = b.stats;
+      console.log(name, stats.mean * 1000 + "ms");
+    }
+
+    console.log();
+    console.log();
+  })
+  .run({ async: true });
