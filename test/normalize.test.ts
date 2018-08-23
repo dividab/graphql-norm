b1e5b059b70c5822bd7c5a809e426c34ecd4925a
Jonas Kello <jonas.kello@gmail.com>
2018-08-23 09:45:35 +0200

    Add failing test

--- test/normalize.test.ts
+++ test/normalize.test.ts
@@ -3,7 +3,7 @@ import { normalize } from "../src/normalize";
 import { tests } from "./shared-test-data";
 import { onlySkip } from "./test-data-utils";
 
-test("normalize() with shared test data", t => {
+test.only("normalize() with shared test data", t => {
   onlySkip(tests).forEach(item => {
     t.test(item.name, st => {
       const actual = normalize(item.query, item.variables, item.response);
