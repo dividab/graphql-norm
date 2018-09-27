112a1348ede5b7abb4aa30f90fa6e33456675498
Jonathan Mourtada <jonathan.mourtada@divid.se>
2018-09-27 15:10:42 +0200

    Add failing test

---
+++ test/denormalize-tests/with-merged-fragment.test.ts
@@ -0,0 +1,80 @@
+import gql from "graphql-tag";
+import { DenormalizeOneTest } from "./denormalize-one-test";
+
+export const test: DenormalizeOneTest = {
+  name: "with merged fragments",
+  only: true,
+  query: gql`
+    query TestQuery {
+      posts {
+        id
+        __typename
+        title
+        author {
+          id
+          __typename
+          name
+        }
+        comments {
+          id
+          __typename
+          commenter {
+            id
+            __typename
+            name
+          }
+        }
+        ...TestFragment
+      }
+    }
+
+    fragment TestFragment on PostType {
+      id
+      title
+      author {
+        id
+        __typename
+        role
+      }
+    }
+  `,
+  partial: false,
+  stale: false,
+  staleEntities: {},
+  response: {
+    data: {
+      posts: [
+        {
+          id: "123",
+          __typename: "Post",
+          author: {
+            id: "1",
+            __typename: "Author",
+            name: "Paul",
+            role: "admin"
+          },
+          title: "My awesome blog post",
+          comments: null
+        }
+      ]
+    }
+  },
+  entities: {
+    ROOT_QUERY: {
+      posts: ["Post;123"]
+    },
+    "Post;123": {
+      id: "123",
+      __typename: "Post",
+      author: "Author;1",
+      title: "My awesome blog post",
+      comments: null
+    },
+    "Author;1": {
+      id: "1",
+      __typename: "Author",
+      name: "Paul",
+      role: "admin"
+    }
+  }
+};
