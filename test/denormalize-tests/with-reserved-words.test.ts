47bc5defcda3cc71063f762907e87ba5fdbb0b3b
Jonathan Mourtada <jonathan.mourtada@divid.se>
2018-09-28 14:29:42 +0200

    enable tests

--- test/denormalize-tests/with-reserved-words.test.ts
+++ test/denormalize-tests/with-reserved-words.test.ts
@@ -3,7 +3,6 @@ import { DenormalizeOneTest } from "./denormalize-one-test";
 
 export const test: DenormalizeOneTest = {
   name: "with reserved keywords",
-  only: true,
   query: gql`
     query TestQuery {
       posts {
