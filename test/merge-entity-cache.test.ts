d85565a615ef17fb349c15a0be094ef3d7ae5275
Victor Widell <victor.widell@divid.se>
2019-05-28 09:22:53 +0200

    Added failing partial value object merge test.

--- test/merge-entity-cache.test.ts
+++ test/merge-entity-cache.test.ts
@@ -76,4 +76,74 @@ test("mergeEntityCache()", t => {
     st.equal(denormalizedResult.partial, false, "Merged data is complete.");
     st.end();
   });
+
+  // When a value-object (an object with no ID, owned by it's parent) is
+  // used, you would expect it to be merged like any other.
+  t.test("partial value objects", st => {
+    const itemA = {
+      name: "",
+      query: gql`
+        query TestQuery {
+          user {
+            id
+            __typename
+            address {
+              __typename
+              city
+            }
+          }
+        }
+      `,
+      response: {
+        data: {
+          user: {
+            id: "123",
+            __typename: "User",
+            address: {
+              __typename: "Address",
+              city: "Los Leones"
+            }
+          }
+        }
+      }
+    };
+
+    const itemB = {
+      name: "",
+      query: gql`
+        query TestQuery {
+          users {
+            id
+            __typename
+            address {
+              __typename
+              region
+            }
+          }
+        }
+      `,
+      response: {
+        data: {
+          users: [
+            {
+              id: "123",
+              __typename: "User",
+              address: {
+                __typename: "Address",
+                region: "Miramar"
+              }
+            }
+          ]
+        }
+      }
+    };
+
+    const entitiesA = normalize(itemA.query, {}, itemA.response);
+    const entitiesB = normalize(itemB.query, {}, itemB.response);
+    const mergedEntities = mergeEntityCache(entitiesA, entitiesB);
+    const denormalizedResult = denormalize(itemA.query, {}, mergedEntities);
+
+    st.equal(denormalizedResult.partial, false, "Merged data is complete.");
+    st.end();
+  });
 });
