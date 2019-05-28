4112874dc8b723aa65a9a556ef0523c2d96f5cf9
Victor Widell <victor.widell@divid.se>
2019-05-28 09:22:08 +0200

    Added merge test.

---
+++ test/merge-entity-cache.test.ts
@@ -0,0 +1,79 @@
+import * as test from "tape";
+import { normalize } from "../src/normalize";
+import gql from "graphql-tag";
+import { mergeEntityCache } from "../src/entity-cache";
+import { denormalize } from "../src/denormalize";
+
+test("mergeEntityCache()", t => {
+  t.test("full value objects", st => {
+    const itemA = {
+      name: "",
+      query: gql`
+        query TestQuery {
+          user {
+            id
+            __typename
+            address {
+              __typename
+              city
+              region
+            }
+          }
+        }
+      `,
+      response: {
+        data: {
+          user: {
+            id: "123",
+            __typename: "User",
+            address: {
+              __typename: "Address",
+              city: "Los Leones",
+              region: "Miramar"
+            }
+          }
+        }
+      }
+    };
+
+    const itemB = {
+      name: "",
+      query: gql`
+        query TestQuery {
+          users {
+            id
+            __typename
+            address {
+              __typename
+              city
+              region
+            }
+          }
+        }
+      `,
+      response: {
+        data: {
+          users: [
+            {
+              id: "123",
+              __typename: "User",
+              address: {
+                __typename: "Address",
+                city: "Los Leones",
+                region: "Miramar"
+              }
+            }
+          ]
+        }
+      }
+    };
+
+    const entitiesA = normalize(itemA.query, {}, itemA.response);
+    const entitiesB = normalize(itemB.query, {}, itemB.response);
+    const mergedEntities = mergeEntityCache(entitiesA, entitiesB);
+    const denormalizedResult = denormalize(itemA.query, {}, mergedEntities);
+
+    st.equal(denormalizedResult.partial, false, "Merged data is complete.");
+    st.end();
+  });
+});
