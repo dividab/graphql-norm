f748c78ee6da3eab687913bf767c35719a5ba0d6
Jonas Kello <jonas.kello@gmail.com>
2019-07-17 13:19:02 +0200

    Test all

--- test/normalize.test.ts
+++ test/normalize.test.ts
@@ -2,7 +2,7 @@ import { normalize } from "../src/normalize";
 import { tests } from "./shared-test-data";
 import { onlySkip } from "./test-data-utils";
 
-describe.only("normalize() with shared test data", () => {
+describe("normalize() with shared test data", () => {
   onlySkip(tests).forEach(item => {
     test(item.name, () => {
       const actual = normalize(item.query, item.variables, item.data);
